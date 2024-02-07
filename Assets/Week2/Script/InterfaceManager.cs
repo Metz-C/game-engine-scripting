b31c4ceef7acbccf2e6bdb62c4963bf335733eb2
Metz-C <anacasti164@gmail.com>
2024-02-07 17:35:53 -0600

    Assignment#2:Redone Calculator due to lost files, Calculator buttons clickable

--- Assets/Week2/Script/InterfaceManager.cs
+++ Assets/Week2/Script/InterfaceManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InterfaceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI label;
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    // Send message on cosole
+   public void PrintMessage()
+   {
+        label.text = "Hello";
         
-    }
+   }
+     
 }
